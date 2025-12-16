47a7c10420f9e9f5111c6cf4e9a2fa0eef311083
Tanakorn Pichai <s6706022511022@email.kmutnb.ac.th>
2025-12-16 11:36:16 +0700

    menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cw4/food.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -26,24 +27,46 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<foodMenu> menu = [
+    foodMenu("กุ้งเผา", "500"),
+    foodMenu("กะเพรา", "50"),
+    foodMenu("ผัดไท", "500"),
+    foodMenu("ก๋วยเตี๋ยว", "4000"),
+    foodMenu("หมูกระทะ", "700"),
+    foodMenu("เนื้อย่าง", "500"),
+  ];
   @override
   Widget build(BuildContext context) {
+    // List<Widget> data = getData();
 
-    throw UnimplementedError();
-  }
-}
-
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-    ),
-    body: Center(
-      child: Column(
-        mainAxisAlignment: .center,
-        children: [const Text('You have pushed the button this many times:')],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("CW4"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-    ),
-  );
+      // body: Center(child: ListView(children: getData())),
+      body: ListView.builder(
+        itemCount: menu.length,
+        itemBuilder: (BuildContext context, int index) {
+          foodMenu food = menu[index];
+          return ListTile(
+            title: Text("เมนูที่ ${index + 1} "),
+            subtitle: Text(food.foodname + "ราคา" + food.foodprice + "บาท"),
+          );
+        },
+      ),
+    );
+  }
+
+  // List<Widget> getData() {
+  //   List<Widget> data = [];
+  //   for (var i = 1; i <= 30; i++) {
+  //     var menu = ListTile(
+  //       title: Text("เมนูที่ $i"),
+  //       subtitle: Text("ราคาของเมนูที่ $i"),
+  //     );
+  //     data.add(menu);
+  //   }
+  //   return data;
+  // }
 }
